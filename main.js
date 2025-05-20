9865f23ffa26c3e46c8ab17c6d01d7243e3c1121
amarisodiq <amarisodiq@gmail.com>
2025-05-20 01:01:21 +0100

    add other sections

--- main.js
+++ main.js
@@ -3,29 +3,7 @@
 // @import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css");
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
-    const navbarCollapse = document.getElementById('navbarSupportedContent');
 
-    navLinks.forEach(link => {
-      link.addEventListener('click', () => {
-        // const bsCollapse = new bootstrap.Collapse(navbarCollapse, {
-        //   toggle: false
-        // });
-
-        const isMobile = window.getComputedStyle(
-          document.querySelector('.navbar-toggler')).display !== 'none';
-
-          if(isMobile){
-            const bsCollapse = new bootstrap.Collapse(navbarCollapse, {
-            toggle: false
-          });
-
-        bsCollapse.hide();
-          }
-    });
-  });
-});
 
 
 // loop
